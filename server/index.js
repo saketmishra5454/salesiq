2c2e057e05fc95e7a76405291323cf5b9107bfbf
Saket Mishra <saketmihsra5454@gmail.com>
2025-07-29 23:49:30 +0530

    add origin to all

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/salesDB";
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 // MongoDB Connection
